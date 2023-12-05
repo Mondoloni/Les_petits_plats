29e42451d69f149a1fb5f29be00bfe1aa44200a8
Mondoloni <113922209+Mondoloni@users.noreply.github.com>
2023-12-05 19:53:15 +0100

    Finalisation boucle native
    Finalisation boucle native

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,89 +1,61 @@
+/* eslint-disable no-undef */
 function displayRecipes(dataRecipes) {
-    const tabIngredients=[];
-    const tabUstensiles=[];
-    const tabAppareils=[];
-    const champRecherche =document.getElementById("header-search-input").value;
-    //On récupére la section des cards des recettes
-    const sectionrecettes = document.getElementById("section-recettes");
-    //On supprime les cards des recettes
-    while (sectionrecettes.firstChild) 
-    {
-        sectionrecettes.removeChild(sectionrecettes.firstChild);
-    }
+  const tabIngredients = [];
+  const tabUstensiles = [];
+  const tabAppareils = [];
+  const champRecherche = document.getElementById("header-search-input").value;
+  // On récupére la section des cards des recettes
+  const sectionrecettes = document.getElementById("section-recettes");
+  // On supprime les cards des recettes
+  while (sectionrecettes.firstChild) {
+    sectionrecettes.removeChild(sectionrecettes.firstChild);
+  }
 
-    if(dataRecipes.length>0)
-    {
-        dataRecipes.forEach((idrecipes) => 
-        {
-            // La fonction est définie dans un autre fichier js
-            // eslint-disable-next-line no-undef
-            const recipesModel = cardRecipes(idrecipes);
-            sectionrecettes.appendChild(recipesModel);
+  if (dataRecipes.length > 0) {
+    dataRecipes.forEach((idrecipes) => {
+      // La fonction est définie dans un autre fichier js
+      // eslint-disable-next-line no-undef
+      const recipesModel = cardRecipes(idrecipes);
+      sectionrecettes.appendChild(recipesModel);
 
-            idrecipes.ingredients.forEach((element)=>
-            {
-                if(tabIngredients.includes(element.ingredient)==false)
-                {
-                    tabIngredients.push(element.ingredient);
-                }
-            });
+      idrecipes.ingredients.forEach((element) => {
+        if (tabIngredients.includes(element.ingredient) === false) {
+          tabIngredients.push(element.ingredient);
+        }
+      });
 
-            idrecipes.ustensils.forEach((element)=>
-            {
-                if(tabUstensiles.includes(element)==false)
-                {
-                    tabUstensiles.push(element);
-                }
-            });
-                    
-            if(tabAppareils.includes(idrecipes.appliance)==false)
-            {
-                tabAppareils.push(idrecipes.appliance)
-            }
-            
-        });
+      idrecipes.ustensils.forEach((element) => {
+        if (tabUstensiles.includes(element) === false) {
+          tabUstensiles.push(element);
+        }
+      });
 
-        // if(maRecherche===undefined)
-        // {console.log("ok")
-            displayDropdown("Ingredients",tabIngredients);
-            displayDropdown("Appareils",tabAppareils);
-            displayDropdown("Ustensiles",tabUstensiles);
-        // }
-        // else{
-            // updateDropdown("Ingredients",maRecherche);
-            // updateDropdown("Appareils",maRecherche);
-            // updateDropdown("Ustensiles",maRecherche);
-        // }
-    }
-    else
-    {
-        const div_no_recipes=document.createElement("div");
-        div_no_recipes.setAttribute("class","no-recipes");
-        const h3_no_recipes=document.createElement("h3");
-        h3_no_recipes.innerHTML=`Aucune recette ne contient ‘${champRecherche} ’ vous pouvez chercher « tarte aux pommes », « poisson », etc.`;
-        div_no_recipes.appendChild(h3_no_recipes);
-        sectionrecettes.appendChild(div_no_recipes);
-    }
-    const h3_total_recettes=document.getElementById("totalRecettes");
-    h3_total_recettes.textContent=dataRecipes.length+" Recettes";
+      if (tabAppareils.includes(idrecipes.appliance) === false) {
+        tabAppareils.push(idrecipes.appliance);
+      }
+    });
+
+    displayDropdown("Ingredients", tabIngredients);
+    displayDropdown("Appareils", tabAppareils);
+    displayDropdown("Ustensiles", tabUstensiles);
+  } else {
+    const divNoRecipes = document.createElement("div");
+    divNoRecipes.setAttribute("class", "no-recipes");
+    const h3NoRecipes = document.createElement("h3");
+    h3NoRecipes.innerHTML = `Aucune recette ne contient ‘${champRecherche} ’ vous pouvez chercher « tarte aux pommes », « poisson », etc.`;
+    divNoRecipes.appendChild(h3NoRecipes);
+    sectionrecettes.appendChild(divNoRecipes);
+  }
+  const h3TotalRecettes = document.getElementById("totalRecettes");
+  h3TotalRecettes.textContent = `${dataRecipes.length} Recettes`;
 }
 
 function init() {
-    
-    const input_header_search=document.getElementById("header-search-input");
-    input_header_search.addEventListener("input", (e) => {
-    //    if(e.currentTarget.value.length>2)
-    //    {
-        tri(e.currentTarget.value)
-    //    }
-    //    else//Dans le cas ou l'on a moins de 3 caractères dans la barre de recherche principale
-    //    {
-    //     tri("");
-    //    }
-    });
-     displayRecipes(recipes);
- }
-
- let listeTags=[];
+  const inputHeaderSearch = document.getElementById("header-search-input");
+  inputHeaderSearch.addEventListener("input", () => {
+    tri();
+  });
+  displayRecipes(recipes);
+}
 
 init();
--- scripts/utils/utils.js
+++ scripts/utils/utils.js
@@ -1,193 +1,95 @@
-function tri(maRecherche)
-{
-    let tabDonnees=[];
-    const champRecherche =document.getElementById("header-search-input").value.toLowerCase();
-    const listeTag=document.getElementsByClassName("span-tag");
-   
-    if(champRecherche.length<3)
-    {
-        for(let i=0;i<recipes.length;i++)
-        {
-            tabDonnees[i]=recipes[i];
+/* eslint-disable default-case */
+/* eslint-disable camelcase */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+function tri() {
+  let tabDonnees = [];
+  const champRecherche = document.getElementById("header-search-input").value.toLowerCase();
+  const listeTag = document.getElementsByClassName("span-tag");
+
+  if (champRecherche.length < 3) {
+    for (let i = 0; i < recipes.length; i++) {
+      tabDonnees[i] = recipes[i];
+    }
+  } else {
+    for (let i = 0; i < recipes.length; i++) {
+      const tempName = recipes[i].name.toLowerCase();
+      const tempDescription = recipes[i].description.toLowerCase();
+
+      let foundInName = false;
+      let foundInDesc = false;
+      let foundIngre = false;
+
+      for (let j = 0; j <= tempName.length - champRecherche.length; j++) {
+        if (tempName.substring(j, j + champRecherche.length) === champRecherche) {
+          foundInName = true;
+          break;
         }
-    }
-    else
-    {
-        for (let i = 0; i <recipes.length ; i++) 
-            {
-                // recipes.length
-                let tempName =recipes[i].name.toLowerCase();
-                let tempDescription =recipes[i].description.toLowerCase();
-                // champRecherche=champRecherche.toLowerCase();
+      }
+      if (!foundInName) {
+        for (let k = 0; k <= tempDescription.length - champRecherche.length; k++) {
+          if (tempDescription.substring(k, k + champRecherche.length) === champRecherche) {
+            foundInDesc = true;
+            break;
+          }
+        }
+      }
 
-                let foundInName=false;
-                let foundInDesc=false;
-                let foundIngre = false;
+      if (!foundInDesc) {
+        for (let l = 0; l < recipes[i].ingredients.length; l++) {
+          const tempIgredients = recipes[i].ingredients[l].ingredient.toLowerCase();
 
-                for(let j=0;j<=tempName.length-champRecherche.length;j++)
-                {
-                    if(tempName.substring(j,j+champRecherche.length)===champRecherche)
-                    {
-                        foundInName = true;
-                        break;
-                    }
-                }
-                if(!foundInName)
-                {
-                    for(let k=0;k<=tempDescription.length-champRecherche.length;k++)
-                    {
-                        if(tempDescription.substring(k,k+champRecherche.length)===champRecherche)
-                        {
-                            foundInDesc=true;
-                            break;
-                        }
-                    }
-                }
-            
-                if(!foundInDesc)
-                {
-                    for (let l = 0; l <recipes[i].ingredients.length ; l++) 
-                    {
-                        // recipes[i].ingredients.length
-                        let tempIgredients =recipes[i].ingredients[l].ingredient.toLowerCase();
-                        
-                        for(let m=0;m<=tempIgredients.length-champRecherche.length;m++)
-                        {
-                            if(tempIgredients.substring(m,m+champRecherche.length)===champRecherche)
-                            {
-                                foundIngre=true;
-                                break;
-                            }
-                        }
-                        // if (
-                        //     recipes[i].ingredients[j].ingredient
-                        //         .toLowerCase()
-                        //         .includes(champRecherche.toLowerCase())
-                        //     ) 
-                        //     {
-                        //         found = true;
-                        //         break;
-                        //     }
-                    }
-                    // if (found) 
-                    // {
-                    //     tabDonnees.push(recipes[i]);
-                    // }
-                }
-                if (foundInName || foundInDesc || foundIngre) 
-                {
-                    tabDonnees[tabDonnees.length]=recipes[i];
-                }
+          for (let m = 0; m <= tempIgredients.length - champRecherche.length; m++) {
+            if (tempIgredients.substring(m, m + champRecherche.length) === champRecherche) {
+              foundIngre = true;
+              break;
             }
-
-    // console.log(tabDonnees)
-//         // console.log(listeTags.length)
-//         let tempRecipes=recipes.slice();
-//         let tempTags =[];
-//         // let tempTags=listeTags.slice();
-//         const liste_tag=document.querySelectorAll('div.div-tag');
-//     const section_label_search=document.getElementById("label-search");
-//     let isPresent=false;
-//    console.log(maRecherche)
-//     liste_tag.forEach((e)=>
-//     {   
-//         tempTags.push(e.innerText);
-//     });
-    
-//     // tempTags.push(maRecherche)
-//         // tempTags.push(maRecherche);
-//         // console.log(tempRecipes);
-//         // console.log(recipes)
-//         console.log(tempTags);
-
-//         for(let h = 0; h < tempTags.length; h++)
-//         {
-//             for (let i = 0; i < tempRecipes.length; i++) 
-//             {
-//                 if (
-//                     tempRecipes[i].name.toLowerCase().includes(tempTags[h].toLowerCase()) ||
-//                     tempRecipes[i].description.toLowerCase().includes(tempTags[h].toLowerCase())
-//                     ) 
-//                 {
-//                     tabDonnees.push(tempRecipes[i]);
-//                 } 
-//                 else 
-//                 {
-//                     let found = false;
-//                     for (let j = 0; j < tempRecipes[i].ingredients.length; j++) 
-//                     {
-//                         if (
-//                             tempRecipes[i].ingredients[j].ingredient
-//                                 .toLowerCase()
-//                                 .includes(maRecherche.toLowerCase())
-//                             ) 
-//                             {
-//                                 found = true;
-//                                 break;
-//                             }
-//                     }
-//                     if (found) 
-//                     {
-//                         tabDonnees.push(tempRecipes[i]);
-//                     }
-//                 }
-//             }
-//             // console.log(tabDonnees)
-//             tempRecipes.slice(tabDonnees);
-//         }
-    // displayRecipes(tabDonnees,maRecherche);
-   
+          }
+        }
+      }
+      if (foundInName || foundInDesc || foundIngre) {
+        tabDonnees[tabDonnees.length] = recipes[i];
+      }
     }
+  }
 
-  
-    for(let i=0;i<listeTag.length;i++)
-    {
-        tabDonnees=tri_Tag(tabDonnees,listeTag[i].getAttribute('data-id'),listeTag[i].innerText);
-    }
-    displayRecipes(tabDonnees);
+  for (let i = 0; i < listeTag.length; i++) {
+    tabDonnees = tri_Tag(tabDonnees, listeTag[i].getAttribute('data-id'), listeTag[i].innerText);
+  }
+  displayRecipes(tabDonnees);
 }
 
-function tri_Tag(listeDonnees,type,tag)
-{
-
- let tempDonnees=[];
- switch (type) 
-        {
-            case 'Ingredients':
-                for(let i=0;i<listeDonnees.length;i++)
-                {
-                    for(let j=0;j<listeDonnees[i].ingredients.length;j++)
-                    {
-                        if(listeDonnees[i].ingredients[j].ingredient===tag)
-                        {
-                            tempDonnees[tempDonnees.length]=listeDonnees[i];
-                        }
-                    }
-                }
-                break;
-            case 'Appareils' :
-                for(let i=0;i<listeDonnees.length;i++)
-                {  
-                        if(listeDonnees[i].appliance===tag)
-                        {
-                            tempDonnees[tempDonnees.length]=listeDonnees[i];
-                        }
-                    
-                }
-                break;
-            case 'Ustensiles' :
-                for(let i=0;i<listeDonnees.length;i++)
-                {
-                    for(let j=0;j<listeDonnees[i].ustensils.length;j++)
-                    {
-                        if(listeDonnees[i].ustensils[j]===tag)
-                        {
-                            tempDonnees[tempDonnees.length]=listeDonnees[i];
-                        }
-                    }
-                }
-                break;
+function tri_Tag(listeDonnees, type, tag) {
+  const tempDonnees = [];
+  switch (type) {
+    case 'Ingredients':
+      for (let i = 0; i < listeDonnees.length; i++) {
+        for (let j = 0; j < listeDonnees[i].ingredients.length; j++) {
+          if (listeDonnees[i].ingredients[j].ingredient === tag) {
+            tempDonnees[tempDonnees.length] = listeDonnees[i];
+          }
         }
-        
- return tempDonnees;
+      }
+      break;
+    case 'Appareils':
+      for (let i = 0; i < listeDonnees.length; i++) {
+        if (listeDonnees[i].appliance === tag) {
+          tempDonnees[tempDonnees.length] = listeDonnees[i];
+        }
+      }
+      break;
+    case 'Ustensiles':
+      for (let i = 0; i < listeDonnees.length; i++) {
+        for (let j = 0; j < listeDonnees[i].ustensils.length; j++) {
+          if (listeDonnees[i].ustensils[j] === tag) {
+            tempDonnees[tempDonnees.length] = listeDonnees[i];
+          }
+        }
+      }
+      break;
+  }
+
+  return tempDonnees;
 }
